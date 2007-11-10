bc0d58db4b0f53a2ee6a3c976ef8e3124c260018
Martin Geisler <mg@daimi.au.dk>
2007-11-10 12:55:06 +0100

    Added missing copyright statement.

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
-#
 # This is the VIFF setup script.
 #
 # For a global install by root, use:    python setup.py install
 # For a local install into ~/opt, use:  python setup.py --home=~/opt
 # For more options, use:                python setup.py --help
+
+# Copyright 2007 Martin Geisler
+#
+# This file is part of VIFF
+#
+# VIFF is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# VIFF is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VIFF in the file COPYING; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
 
 from distutils.command.sdist import sdist
 from distutils.core import setup
