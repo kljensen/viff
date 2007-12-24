f9000f34ef6c963ceb67903d3c9c8b9279755d09
Martin Geisler <mg@daimi.au.dk>
2007-12-24 16:21:11 +0100

    Created a new unified benchmark program.
    The benchmark can test multiplications, the SCET comparison, or the
    newer comparison, and the operations can be scheduled to run in
    parallel or sequential. This makes the multiplication_benchmark.py and
    comparison_benchmark.py files obsolete.

---
+++ apps/benchmark.py
@@ -0,0 +1,206 @@
+#!/usr/bin/python
+
+# Copyright 2007 Martin Geisler
+#
+# This file is part of VIFF
+#
+# VIFF is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# VIFF is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VIFF in the file COPYING; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+# This example benchmarks multiplications and comparisons. Run it with
+# '--help' on the command line to see the available options.
+#
+# Two lists of shares are created and the numbers are multiplied or
+# compared pair-wise. This can be scheduled in two ways: parallel or
+# sequentially. Parallel execution looks like this:
+#
+#      *        *             *   
+#     / \      / \    ...    / \  
+#   a1   b1  a2   b2       an   bn
+#
+# Given sufficient bandwidth all operations should be executed in
+# parallel.
+#
+# Sequential execution looks like this:
+#
+#      *
+#     / \
+#   a1   b1
+#        
+#      *
+#     / \
+#   a2   b2
+#
+#     ...
+#
+#      *
+#     / \
+#   an   bn
+#
+# Here the next operation is only started when the previous has
+# finished.
+#
+# In all cases the time reported is measured from the moment when the
+# operands are ready until all the results are ready.
+
+import time
+from optparse import OptionParser
+
+from twisted.internet import reactor
+
+from gmpy import mpz
+from viff.field import GF
+from viff.runtime import Runtime, create_runtime, gather_shares
+from viff.config import load_config
+
+last_timestamp = time.time()
+start = 0
+
+def record_start():
+    global start
+    start = time.time()
+    print "*" * 64
+    print "Started"
+
+def record_stop(_):
+    stop = time.time()
+    print
+    print "Total time used: %.3f sec" % (stop-start)
+    print "Time per operation: %.3f ms" % (1000*float(stop-start) / count)
+    print "*" * 6
+
+parser = OptionParser()
+parser.add_option("-m", "--modulus",
+                  help="lower limit for modulus (can be an expression)")
+parser.add_option("-c", "--count", type="int",
+                  help="number of operations")
+parser.add_option("-o", "--operation", type="choice",
+                  choices=["mul", "comp", "compII"],
+                  help="operation to benchmark, one of 'mul', 'comp', 'compII'")
+parser.add_option("-p", "--parallel", action="store_true",
+                  help="execute operations in parallel")
+parser.add_option("-s", "--sequential", action="store_false", dest="parallel",
+                  help="execute operations in sequence")
+
+parser.set_defaults(modulus="30916444023318367583", count=10,
+                    operation="mul", parallel=True)
+
+# Add standard VIFF options.
+Runtime.add_options(parser)
+
+(options, args) = parser.parse_args()
+
+if len(args) == 0:
+    parser.error("you must specify a config file")
+
+id, players = load_config(args[0])
+
+modulus = eval(options.modulus, {}, {})
+
+if modulus < 0:
+    parser.error("modulus is negative: %d" % modulus)
+
+prime = long(mpz(modulus-1).next_prime())
+
+if str(prime) != options.modulus:
+    print "Using %d as modulus" % prime
+    if prime != modulus:
+        print "Adjusted from %d" % modulus
+
+Zp = GF(prime)
+count = options.count
+print "I am player %d, will %s %d numbers" % (id, options.operation, count)
+
+# Defining the protocol as a class makes it easier to write the
+# callbacks in the order they are called. This class is a base class
+# that executes the protocol by calling the run_test method.
+class Benchmark:
+    def __init__(self, rt, operation):
+        print "Runtime ready, starting protocol"
+        self.rt = rt
+        self.operation = operation
+        self.a_shares = [self.rt.prss_share_random(Zp) for _ in range(count)]
+        self.b_shares = [self.rt.prss_share_random(Zp) for _ in range(count)]
+        shares_ready = gather_shares(self.a_shares + self.b_shares)
+        shares_ready.addCallback(self.sync_test)
+
+    def sync_test(self, _):
+        print "Synchronizing test start."
+        sync = self.rt.synchronize()
+        sync.addCallback(self.run_test)
+
+    def run_test(self, _):
+        raise NotImplemented("Override this abstract method in a sub class.")
+
+    def finished(self, _):
+        print "Finished, synchronizing shutdown."
+        sync = self.rt.synchronize()
+        sync.addCallback(self.shutdown)
+        
+    def shutdown(self, _):
+        print "Shutdown."
+        self.rt.shutdown()
+        print "Stopped VIFF Runtime."
+
+# This class implements a benchmark where run_test executes all
+# operations in parallel.
+class ParallelBenchmark(Benchmark):
+    def run_test(self, _):
+        print "Starting parallel test."
+        c_shares = []
+        record_start()
+        while self.a_shares and self.b_shares:
+            a = self.a_shares.pop()
+            b = self.b_shares.pop()
+            c_shares.append(self.operation(a, b))
+
+        done = gather_shares(c_shares)
+        done.addCallback(record_stop)
+        done.addCallback(self.finished)
+
+# A benchmark where the operations are executed one after each other.
+class SequentialBenchmark(Benchmark):
+    def run_test(self, _):
+        print "Starting sequential test."
+        record_start()
+        self.single_operation(None)
+
+    def single_operation(self, _):
+        if self.a_shares and self.b_shares:
+            a = self.a_shares.pop()
+            b = self.b_shares.pop()
+            c = self.operation(a, b)
+            c.addCallback(self.single_operation)
+        else:
+            record_stop(None)
+            self.finished(None)
+
+if options.operation == "mul":
+    operation = lambda a, b: a * b
+elif options.operation == "comp":
+    operation = lambda a, b: a.runtime.greater_than(a, b, Zp)
+elif options.operation == "compII":
+    operation = lambda a, b: a.runtime.greater_thanII(a, b, Zp)
+
+if options.parallel:
+    benchmark = ParallelBenchmark
+else:
+    benchmark = SequentialBenchmark
+
+pre_runtime = create_runtime(id, players, (len(players) -1)//2, options)
+pre_runtime.addCallback(benchmark, operation)
+
+print "#### Starting reactor ###"
+reactor.run()
