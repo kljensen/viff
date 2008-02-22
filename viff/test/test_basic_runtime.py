140ae272a067b15cd888d94f98fbee67f68daa00
Martin Geisler <mg@daimi.au.dk>
2008-02-22 19:34:13 +0100

    Removed yet another unused import.

--- viff/test/test_basic_runtime.py
+++ viff/test/test_basic_runtime.py
@@ -17,7 +17,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-from twisted.internet.defer import Deferred, gatherResults, FirstError
+from twisted.internet.defer import Deferred, gatherResults
 from twisted.trial.unittest import TestCase, FailTest
 
 from viff.test.util import RuntimeTestCase, protocol
