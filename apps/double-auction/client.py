193ccda80ed4980e6d4a2e62411bbfd84bae97b5
Martin Geisler <mg@daimi.au.dk>
2008-04-03 21:50:39 +0200

    Switching to GNU LGPL version 3.
    Since VIFF is more like a library than a compiler, we think that it is
    better to license it under the LGPL instead of the GPL. This allows
    everybody to build applications that use VIFF without licensing those
    applications under the LGPL too. But if you modify and distribute
    VIFF, then you still need to provide the modified code to people under
    the terms of the LGPL.

--- apps/double-auction/client.py
+++ apps/double-auction/client.py
@@ -2,20 +2,18 @@
 #
 # This file is part of VIFF, the Virtual Ideal Functionality Framework.
 #
-# VIFF is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# VIFF is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License (LGPL) as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# VIFF is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# VIFF is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with VIFF in the file COPYING; if not, write to the Free
-# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-# 02110-1301 USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with VIFF. If not, see <http://www.gnu.org/licenses/>.
 
 from twisted.internet import gtk2reactor
 gtk2reactor.install()
--- setup.py
+++ setup.py
@@ -8,20 +8,18 @@
 #
 # This file is part of VIFF, the Virtual Ideal Functionality Framework.
 #
-# VIFF is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# VIFF is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License (LGPL) as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# VIFF is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# VIFF is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with VIFF in the file COPYING; if not, write to the Free
-# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-# 02110-1301 USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with VIFF. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.command.sdist import sdist
 from distutils.core import setup
@@ -89,7 +87,7 @@ that an operation starts as soon as the operands are ready.
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Communications',
