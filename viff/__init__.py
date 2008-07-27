5dcd0d1446100390c2819ef5394c265a215ab3a8
Martin Geisler <mg@daimi.au.dk>
2008-07-27 17:43:42 +0200

    New function for getting full release number.

--- viff/__init__.py
+++ viff/__init__.py
@@ -126,3 +126,33 @@ applications distributed in the C{apps} directory.
 
 __version__ = '0.6'
 __license__ = 'GNU LGPL'
+
+def release():
+    """Get the full release number.
+
+    If Mercurial is available, "hg identify" will be used to determine
+    the state of the repository and a string of the form ``x.y-hash``
+    is returned where ``hash`` is the changeset ID or tag. If the tag
+    is the same as ``__version__``, then ``__version__`` is simply
+    returned.
+    """
+    try:
+        from subprocess import Popen, PIPE
+        p = Popen(["hg", "identify"], stdout=PIPE)
+        stdout, _ = p.communicate()
+        if p.returncode != 0:
+            extra = "unknown"
+        else:
+            parts = stdout.split()
+            if len(parts) == 1 or parts[1] == "tip":
+                # No tag for this changeset or only "tip".
+                extra = parts[0]
+            else:
+                extra = parts[1]
+    except OSError:
+        extra = "unknown"
+
+    if extra == __version__:
+        return __version__
+    else:
+        return "%s-%s" % (__version__, extra)
