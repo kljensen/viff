1a89a521e2b691b68097c01b7a30d41a32b64f5c
Martin Geisler <mg@daimi.au.dk>
2008-09-26 21:52:00 +0200

    Save profile results in player-X.prof files.

--- viff/runtime.py
+++ viff/runtime.py
@@ -1124,7 +1124,7 @@ def create_runtime(id, players, threshold, options=None, runtime_class=Runtime):
         # http://bugs.python.org/issue1375 since the start and stop
         # calls are in different stack frames.
         import hotshot
-        prof = hotshot.Profile("viff-%d.prof" % id)
+        prof = hotshot.Profile("player-%d.prof" % id)
         old_run = reactor.run
         def new_run(*args, **kwargs):
             print "Starting reactor with profiling"
@@ -1134,7 +1134,7 @@ def create_runtime(id, players, threshold, options=None, runtime_class=Runtime):
             import hotshot.stats
             print "Loading profiling statistics...",
             sys.stdout.flush()
-            stats = hotshot.stats.load("viff-%d.prof" % id)
+            stats = hotshot.stats.load("player-%d.prof" % id)
             print "done."
             print
             stats.strip_dirs()
