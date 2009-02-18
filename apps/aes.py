22cbdaf89ce34402679fb28a4c1f6ace91a3970a
Marcel Keller <mkeller@cs.au.dk>
2009-02-18 20:13:57 +0100

    Encrypting several blocks in parallel possible in AES example application.

--- apps/aes.py
+++ apps/aes.py
@@ -48,6 +48,9 @@ parser.add_option("-m", "--masking", action="store_false",
 parser.set_defaults(exponentiation=1)
 parser.add_option("-o", "--at-once", action="store_true",help="Prepare "
                   "the whole computation at once instead of round-wise.")
+parser.add_option("-c", "--count", action="store", type="int",
+                  help="Number of blocks to encrypt. Defaults to 1.")
+parser.set_defaults(count=1)
 
 # Add standard VIFF options.
 Runtime.add_options(parser)
@@ -64,8 +67,12 @@ def encrypt(_, rt, key):
     print "Started at %f." % start
 
     aes = AES(rt, 192, use_exponentiation=options.exponentiation)
-    ciphertext = aes.encrypt("a" * 16, key, True, 
-                             prepare_at_once=options.at_once)
+
+    ciphertext = []
+
+    for i in range(options.count):
+        ciphertext += aes.encrypt("a" * 16, key, True, 
+                                  prepare_at_once=options.at_once)
 
     opened_ciphertext = [rt.open(c) for c in ciphertext]
 
