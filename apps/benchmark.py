26d0ecee46693fb8817862b2468bdd41ae4c3a84
Martin Geisler <mg@cs.au.dk>
2009-10-16 18:42:14 +0200

    benchmark: fixed copyright year in license header

--- apps/benchmark.py
+++ apps/benchmark.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2007, 2008 VIFF Development Team.
+# Copyright 2007, 2008, 2009 VIFF Development Team.
 #
 # This file is part of VIFF, the Virtual Ideal Functionality Framework.
 #
