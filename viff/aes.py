abadbf2b30f34e8fd225049619088c0c7c83d936
Marcel Keller <mkeller@cs.au.dk>
2009-10-28 20:23:26 +0100

    aes: Simplified callbacks in masked exponentiation.

--- viff/aes.py
+++ viff/aes.py
@@ -155,7 +155,8 @@ class AES:
         return result
 
     def invert_by_masked_exponentiation(self, byte):
-        def add_and_multiply(masked_powers, random_powers, prep):
+        def add_and_multiply(masked_byte, random_powers, prep):
+            masked_powers = self.runtime.powerchain(masked_byte, 7)
             byte_powers = map(operator.add, masked_powers, random_powers)[1:]
             if prep:
                 byte_powers = [Share(self.runtime, GF256, value)
@@ -166,10 +167,8 @@ class AES:
 
         random_powers, prep = self.runtime.prss_powerchain()
         masked_byte = self.runtime.open(byte + random_powers[0])
-        masked_powers = self.runtime.schedule_callback(masked_byte,
-            lambda masked_byte: self.runtime.powerchain(masked_byte, 7))
         return self.runtime.schedule_callback(
-            masked_powers, add_and_multiply, random_powers, prep)
+            masked_byte, add_and_multiply, random_powers, prep)
 
     def invert_by_exponentiation(self, byte):
         byte_2 = byte * byte
