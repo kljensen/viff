4d5f89a2560c4daeccff75526178a443b7957d37
Martin Geisler <mg@cs.au.dk>
2009-12-14 13:01:24 +0100

    doc: added inheritance diagrams

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,8 @@ os.environ['VIFF_NO_WRAP'] = 'yes'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.ifconfig',
+              'sphinx.ext.inheritance_diagram']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
