2a19227a61e6f60d5f28b141f0efc7fa4645aab2
Thomas P Jakobsen <tpj@cs.au.dk>
2010-09-23 10:07:35 +0200

    BeDOZa: Moved tests to separate folder.

---
+++ viff/test/bedoza/__init__.py
@@ -0,0 +1,16 @@
+# Copyright 2007, 2010 VIFF Development Team.
+#
+# This file is part of VIFF, the Virtual Ideal Functionality Framework.
+#
+# VIFF is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License (LGPL) as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# VIFF is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General
+# Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with VIFF. If not, see <http://www.gnu.org/licenses/>.
